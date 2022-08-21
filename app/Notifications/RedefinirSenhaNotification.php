430c509f3e0d38ce7b4d0673050494305669b4d8
Luiz Henrique Santos <luiz.santos85@gmail.com>
2022-08-21 16:30:21 -0300

    alteracao url email reset

--- app/Notifications/RedefinirSenhaNotification.php
+++ app/Notifications/RedefinirSenhaNotification.php
@@ -44,7 +44,7 @@ class RedefinirSenhaNotification extends Notification
      */
     public function toMail($notifiable)
     {
-        $url = "http://localhost:8000/password/reset/{$this->token}?email={$this->email}";
+        $url = config('app.url')."/password/reset/{$this->token}?email={$this->email}";
         $expire = config('auth.passwords.' . config('auth.defaults.passwords') . '.expire');
         $saudacao = "Olá {$this->name}";
 
