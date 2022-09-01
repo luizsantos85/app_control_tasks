ba4cf3d4f22968474d91980428d4ff01a273bac1
Luiz Henrique Santos <luiz.santos85@gmail.com>
2022-09-01 20:51:26 -0300

    ajuste export

--- app/Exports/TasksExport.php
+++ app/Exports/TasksExport.php
@@ -4,14 +4,27 @@ namespace App\Exports;
 
 use App\Models\Task;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class TasksExport implements FromCollection
+class TasksExport implements FromCollection, WithHeadings
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
         return auth()->user()->tasks()->get();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID Tarefa',
+            'ID Usuário',
+            'Título/Descrição',
+            'Data Limite Conclusão',
+            'Data de Criação',
+            'Data de Atualização'
+        ];
     }
 }
